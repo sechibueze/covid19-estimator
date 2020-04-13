57ac0807896f7ebf363a3385c23807a177d51645
Samuel Chibueze <lightworthng@gmail.com>
2020-04-13 13:02:03 +0100

    adjust decimal places in logs

--- utils/logger.js
+++ utils/logger.js
@@ -6,7 +6,7 @@ const getActualRequestDurationInMilliseconds = start => {
   const NS_PER_SEC = 1e9; //  convert to nanoseconds
   const NS_TO_MS = 1e6; // convert to milliseconds
   const diff = process.hrtime(start);
-  return (diff[0] * NS_PER_SEC + diff[1]) / NS_TO_MS;
+  return Math.ceil(diff[0] * NS_PER_SEC + diff[1]) / NS_TO_MS;
 };
 
 
@@ -28,10 +28,11 @@ let demoLogger = (req, res, next) => { //middleware function
   let url = req.url;
   let status = res.statusCode;
   const start = process.hrtime();
-  const durationInMilliseconds = getActualRequestDurationInMilliseconds(start);
+  const durationInMilliseconds = getActualRequestDurationInMilliseconds(start).toFixed(2);
+  console.log('durationInMilliseconds ::', durationInMilliseconds);
   // let log = `[${formatted_date}] ${method}:${url} ${status} ${durationInMilliseconds.toLocaleString()} ms`;
   let log = `${method}  ${url}  ${status}  ${durationInMilliseconds.toLocaleString()}ms`;
-  console.log(log);
+  // console.log(log);
   fs.appendFile("request_logs.txt", log + "\n", err => {
     if (err) {
       console.log(err);
